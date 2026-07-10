45ced49ed57dedd2ae3157a9f7ed585b3b7d18b6
10pauloacre-creator <10pauloacre-creator@users.noreply.github.com>
2026-07-10 14:12:58 +0000

    deploy: 5ba93149b87ee895308897dbb9cf4826e9007651

---
+++ assets/js/casavequia-grade-seeds.js
@@ -0,0 +1,297 @@
+window.CASAVEQUIA_GRADE_SEEDS = {
+  "pc_alunos_1serie_2026_v1": {
+    "1": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3, "prova": 5 } }
+    },
+    "2": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "3": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } }
+    },
+    "4": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4.5 } }
+    },
+    "5": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "6": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 3 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "7": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4 } }
+    },
+    "8": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 4.5 } }
+    },
+    "9": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4.5 } }
+    },
+    "10": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4.5 } }
+    },
+    "11": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "12": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 3.5 } }
+    },
+    "13": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 2.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3, "prova": 4.5 } }
+    },
+    "14": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 3 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3.5 } }
+    },
+    "15": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4 } }
+    },
+    "16": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 5 } }
+    }
+  },
+  "pc_alunos_2serie_2026_v1": {
+    "1": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "2": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 5 } }
+    },
+    "3": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4.5 } }
+    },
+    "4": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 3.5 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 4 } }
+    },
+    "5": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 4.5 } }
+    },
+    "6": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3 } }
+    },
+    "7": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "8": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3.5 } }
+    },
+    "9": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4.5 } }
+    },
+    "10": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4 } }
+    },
+    "11": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 2.5, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 4 } }
+    },
+    "12": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 3.5 } }
+    },
+    "13": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 4 } }
+    },
+    "14": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 4 } }
+    },
+    "15": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 2.5 } }
+    },
+    "16": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4 } }
+    },
+    "17": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4.5 } }
+    },
+    "18": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 2.5 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 5 } }
+    },
+    "19": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 4 } }
+    },
+    "20": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 2.5, "prova": 5 } }
+    },
+    "21": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 4.5 } }
+    },
+    "22": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 3, "prova": 4.5 } }
+    },
+    "23": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 2.5 } }
+    },
+    "24": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3 } }
+    },
+    "25": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4.5 } }
+    },
+    "26": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 3.5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 4.5 } }
+    }
+  },
+  "pc_alunos_3serie_2026_v1": {
+    "1": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 4.5 } }
+    },
+    "2": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3 } },
+      "Artes": { "1": { "trabalhos": 3, "prova": 5 } }
+    },
+    "3": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 3.5 } }
+    },
+    "4": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 4 } }
+    },
+    "5": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 3.5 } }
+    },
+    "6": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 2.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 5, "prova": 4 } }
+    },
+    "7": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 4.5 } }
+    },
+    "8": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 3.5 } }
+    },
+    "9": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3.5 } }
+    },
+    "10": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 4 } }
+    },
+    "11": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "12": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 5 } }
+    },
+    "13": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4.5, "prova": 3.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3.5 } }
+    },
+    "14": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 5, "prova": 3.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3.5 } }
+    },
+    "15": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 2.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 4, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 3.5, "prova": 4.5 } }
+    },
+    "16": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 5, "prova": 4 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    },
+    "17": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4.5, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4 } },
+      "Artes": { "1": { "trabalhos": 4, "prova": 5 } }
+    },
+    "18": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 4, "prova": 3.5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 4.5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 3.5 } }
+    },
+    "19": {
+      "Trilhas de Linguagens": { "1": { "trabalhos": 3.5, "prova": 5 } },
+      "Trilhas de C. Humanas": { "1": { "trabalhos": 3.5, "prova": 5 } },
+      "Artes": { "1": { "trabalhos": 4.5, "prova": 5 } }
+    }
+  }
+};
