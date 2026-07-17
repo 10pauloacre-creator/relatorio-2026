d0e059caae77943e39d7903c30cf2924cb6f2837
10pauloacre-creator <10pauloacre-creator@users.noreply.github.com>
2026-07-17 19:40:50 +0000

    deploy: 15caa200f83abe286ee97085965f040e7ade8a91

--- assets/app-build.js
+++ assets/app-build.js
@@ -2,13 +2,13 @@ window.__RELATORIOS_APP_BUILD__ = {
   "appName": "Relatórios",
   "appId": "com.relatoriosdiarios.app",
   "channel": "debug",
-  "versionName": "1.0.9",
-  "versionCode": 11,
+  "versionName": "1.0.10",
+  "versionCode": 12,
   "notes": [
     "Atualização estrutural do aplicativo."
   ],
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-07-17T18:43:01.780Z"
+  "generatedAt": "2026-07-17T19:40:45.755Z"
 };
