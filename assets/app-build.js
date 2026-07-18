47697e67721f7ed0cc278229df37dbd1c0468798
10pauloacre-creator <10pauloacre@gmail.com>
2026-07-17 22:10:15 -0500

    d

--- assets/app-build.js
+++ assets/app-build.js
@@ -2,13 +2,13 @@ window.__RELATORIOS_APP_BUILD__ = {
   "appName": "Relatórios",
   "appId": "com.relatoriosdiarios.app",
   "channel": "debug",
-  "versionName": "1.0.10",
-  "versionCode": 12,
+  "versionName": "1.0.11",
+  "versionCode": 13,
   "notes": [
-    "Corrige as notificacoes do contador do Claude e adiciona o pedido de permissao do Android."
+    "Atualização estrutural do aplicativo."
   ],
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-07-17T19:39:09.101Z"
+  "generatedAt": "2026-07-18T03:03:50.600Z"
 };
