30f65f006cd477bef16d11704e31ff6849deccb2
10pauloacre-creator <10pauloacre@gmail.com>
2026-08-04 09:01:00 -0500

    g

--- assets/app-build.js
+++ assets/app-build.js
@@ -10,5 +10,5 @@ window.__RELATORIOS_APP_BUILD__ = {
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-07-30T20:02:54.389Z"
+  "generatedAt": "2026-08-03T12:53:23.066Z"
 };
