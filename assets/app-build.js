1f306f9a331e54c0f072d2ab1c6bd99fa98da31a
10pauloacre-creator <10pauloacre-creator@users.noreply.github.com>
2026-08-05 20:37:14 +0000

    deploy: 4f940c95f9e355de89cc37401a73c951435163c8

--- assets/app-build.js
+++ assets/app-build.js
@@ -10,5 +10,5 @@ window.__RELATORIOS_APP_BUILD__ = {
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-08-05T13:21:42.389Z"
+  "generatedAt": "2026-08-05T20:37:08.660Z"
 };
