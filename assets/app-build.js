f4a7b7b0d50c6b9e44526b2d48842f19ab02ce26
10pauloacre-creator <10pauloacre@gmail.com>
2026-08-19 11:34:27 -0500

    Ajusta registros manuais do 8o e 9o ano

--- assets/app-build.js
+++ assets/app-build.js
@@ -10,5 +10,5 @@ window.__RELATORIOS_APP_BUILD__ = {
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-08-19T16:28:33.199Z"
+  "generatedAt": "2026-08-19T16:33:59.238Z"
 };
