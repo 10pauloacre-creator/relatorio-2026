24f8095943ac8fd071be60f6bedd52e33a328027
10pauloacre-creator <10pauloacre@gmail.com>
2026-08-27 21:00:40 -0500

    Adiciona lancamentos de 28 de agosto

--- assets/app-build.js
+++ assets/app-build.js
@@ -10,5 +10,5 @@ window.__RELATORIOS_APP_BUILD__ = {
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-08-27T12:27:26.631Z"
+  "generatedAt": "2026-08-28T02:00:26.615Z"
 };
