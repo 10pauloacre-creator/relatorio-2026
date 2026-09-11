829746b241c8e57f17f85021ab3a80c309f8cc59
10pauloacre-creator <10pauloacre@gmail.com>
2026-09-11 09:28:44 -0500

    Adiciona aulas de arte de 11 de setembro

--- assets/app-build.js
+++ assets/app-build.js
@@ -10,5 +10,5 @@ window.__RELATORIOS_APP_BUILD__ = {
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-09-10T12:46:06.922Z"
+  "generatedAt": "2026-09-11T14:28:29.394Z"
 };
