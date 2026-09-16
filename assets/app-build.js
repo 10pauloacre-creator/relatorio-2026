2a040b2e618c39a4eba161385297d85092ba11aa
10pauloacre-creator <10pauloacre@gmail.com>
2026-09-16 15:59:06 -0500

    Atualiza contraturno de arte de 9 de setembro

--- assets/app-build.js
+++ assets/app-build.js
@@ -10,5 +10,5 @@ window.__RELATORIOS_APP_BUILD__ = {
   "downloadPageUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/",
   "latestJsonUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.json",
   "latestScriptUrl": "https://10pauloacre-creator.github.io/relatorio-2026/downloads/latest.js",
-  "generatedAt": "2026-09-16T13:19:45.098Z"
+  "generatedAt": "2026-09-16T20:58:54.225Z"
 };
